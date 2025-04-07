aabd9d4a2d7138f5c75beee895c457a11abf80de
Christian Soltenborn <christian.soltenborn@gmail.com>
2025-04-07 12:23:21 +0200

    Blatt 2 A3 b)

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,5 +1,33 @@
 public class Rectangle {
 
+    public static Rectangle copy(final Rectangle toCopy) {
+        return Rectangle.newRectangle(toCopy.x, toCopy.y, toCopy.width, toCopy.height);
+    }
+
+    public static Rectangle newRectangle(
+            final int x,
+            final int y,
+            final int width,
+            final int height
+    ) {
+        if (width < 0 || height < 0) {
+            Utility.error(
+                    String.format(
+                            "Invalid rectangle dimensions: height %d, width %d",
+                            height,
+                            width
+                    )
+            );
+            return null;
+        }
+        final Rectangle res = new Rectangle();
+        res.x = x;
+        res.y = y;
+        res.width = width;
+        res.height = height;
+        return res;
+    }
+
     private int height;
 
     private int width;
